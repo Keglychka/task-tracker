416b8c5d2c346a28dca9e5349b1675a274fc41a2
Aleksandr <sannikov.sasha73@yandex.ru>
2025-12-27 16:52:46 +0400

    Добавил роуты протестирвоать

--- task-tracker/src/App.jsx
+++ task-tracker/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import TaskTracker from './components/TaskTracker/TaskTracker'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TaskDetail from './components/TaskTracker/TaskDetail'
 
 function App() {
 
   return (
-    <TaskTracker />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TaskTracker />} />
+        <Route path='/tasks/:taskId' element={<TaskDetail />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
